e5256aed69b0cc058bf90ef06cb83103824bbd92
Aayush Kumar Shrivastava <aayushs894@gmail.com>
2024-05-26 10:19:18 +0530

    abcd

--- .vercel/build.sh
+++ .vercel/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ensure the build tools are installed
-pip install setuptools
+python3.9 -m pip install --upgrade pip setuptools
 
 # Proceed with the usual installation
-pip install --disable-pip-version-check --target . --upgrade -r requirements.txt
+python3.9 -m pip install --disable-pip-version-check --target . --upgrade -r requirements.txt
